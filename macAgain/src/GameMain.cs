e8a19856c206c6744242ca10dda8a31505263299
dannamachi <serenita15@gmail.com>
2019-04-12 17:47:22 +1000

    Revert winThatWorks

--- macAgain/src/GameMain.cs
+++ macAgain/src/GameMain.cs
@@ -11,7 +11,10 @@ namespace MyGame
             //Open the game window
             SwinGame.OpenGraphicsWindow("GameMain", 800, 600);
             SwinGame.ShowSwinGameSplashScreen();
+<<<<<<< HEAD
             Shape thing = new Shape("Hello will this work?");
+=======
+>>>>>>> parent of f3f9608... Merge pull request #4 from dannamachi/try-cross-platform
             
             //Run the game loop
             while(false == SwinGame.WindowCloseRequested())
@@ -22,7 +25,7 @@ namespace MyGame
                 //Clear the screen and draw the framerate
                 SwinGame.ClearScreen(Color.White);
                 SwinGame.DrawFramerate(0,0);
-
+                
                 //Draw onto the screen
                 SwinGame.DrawText(thing.Thing, Color.Black, 50, 50);
                 SwinGame.RefreshScreen(60);
