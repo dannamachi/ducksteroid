ea54fba008adc0b184ab28c301488d5e908badec
peterdu98 <peterhung998@gmail.com>
2019-04-13 22:14:10 +1000

    * Ship.cs: Move and Shoot bullets, can't Rotate
    * Bullet.cs: Move, but can't move correcly once the
      ship incline

--- macAgain/src/Bullet.cs
+++ macAgain/src/Bullet.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using SwinGameSDK;
+
 namespace MyGame
 {
-    public class Bullet
+    public class Bullet : Shape
     {
-        public Bullet ()
+        private int _radius;
+        private Vector _velocity;
+        private Point2D _position;
+
+        //Constructors
+        public Bullet (Color clr, float x, float y, int radius) : base ("bullet", clr)
         {
+            _radius = radius;
+            _position.X = x;
+            _position.Y = y;
+        }
+        public Bullet () : this (Color.White,0,0, 3)
+        {
+        }
+
+        //Properties
+        public int Radius { set => _radius = value; get => _radius; }
+
+        //Methods
+        //Draw a bullet
+        public override void Draw () => SwinGame.FillCircle (Color, _position.X, _position.Y, Radius);
+
+        //Bullet hits something
+        public override bool IsAt (Point2D pt) => SwinGame.PointInCircle (pt, X, Y, Radius);
+
+        //Bullet move
+        public void Move (float x, float y)
+        {
+            _velocity.Y = -5;
+            _position = SwinGame.AddVectors (_position, _velocity);
         }
     }
 }
--- macAgain/src/Ship.cs
+++ macAgain/src/Ship.cs
@@ -1,10 +1,51 @@
 ﻿using System;
+using SwinGameSDK;
+using System.Collections.Generic;
+
 namespace MyGame
 {
-    public class Ship
+    public class Ship : Shape
     {
-        public Ship ()
+        private List<Bullet> _bullets = new List<Bullet> ();
+        private Triangle _triangle;
+
+        //Constructors
+        public Ship (Color clr, float x, float y) : base ("ship", clr)
         {
+            X = x;
+            Y = y;
+            _triangle = SwinGame.CreateTriangle (X, Y, X - 15, Y + 20, X + 15, Y + 20);
+        }
+        public Ship () : this (Color.White, 0,0)
+        {
+        }
+
+        //Properties
+        public Triangle TriangleShip { 
+            get => _triangle;
+            set => _triangle = value;
+        }
+
+        //Methods
+        //Draw the the ship
+        public override void Draw () => SwinGame.FillTriangle(Color, TriangleShip);
+
+        //If something touches within the ship's area
+        public override bool IsAt (Point2D pt) => SwinGame.PointInTriangle (pt, TriangleShip);
+
+        //Update the location of the ship
+        public void Update ()
+        {
+
+        }
+
+        //Add bullet to the list of bullets
+        public void AddBullet () => _bullets.Add (new Bullet (Color.White,X,Y,3));
+        //Shot bullet
+        public void Shoot ()
+        {
+            _bullets.ForEach (b => b.Draw ());
+            _bullets.ForEach (b => b.Move (X, Y));
         }
     }
 }
