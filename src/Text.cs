3546014cfd1fd44e5a1c0311518e4f6d1b1cbaae
dannamachi <serenita15@gmail.com>
2019-04-13 21:22:36 +1000

    Add basic Text

---
+++ src/Text.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SwinGameSDK;
+
+namespace MyGame.src
+{
+    public class Text : Shape
+    {
+        //fields
+        private string text;
+        private Font theFont;
+        private Rectangle box;
+        //constructors
+        public Text (string stuff, Color color, Font font, int x, int y) : base ()
+        {
+            box = new Rectangle();
+            theFont = font;
+            text = stuff;
+        }
+        public Text () : this ("",)
+        //properties
+        //methods
+    }
+}
