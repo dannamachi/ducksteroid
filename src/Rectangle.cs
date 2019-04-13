0a6b89f9b8c7ab80f10e82ca725a8470e1bbded5
dannamachi <serenita15@gmail.com>
2019-04-13 19:55:28 +1000

    Fix shape and add rectangle
    add setter for shape's properties
    add some basic things for rectangle class (not implement abstract stuff)

---
+++ src/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SwinGameSDK;
+
+namespace MyGame.src
+{
+    public class Rectangle : Shape
+    {
+        //fields
+        private int width;
+        private int height;
+        //constructors
+        public Rectangle (Color clr, float x, float y, int w, int h) : base (clr)
+        {
+            X = x;
+            Y = y;
+            width = w;
+            height = h;
+        }
+    }
+}
--- src/Shape.cs
+++ src/Shape.cs
@@ -21,8 +21,8 @@ namespace MyGame.src
         }
         public Shape() : this(Color.White) { }
         //properties
-        public float X { get => x; }
-        public float Y { get => y; }
+        public float X { get => x; set => x = value; }
+        public float Y { get => y; set => y = value; }
         public bool Selected { get => selected; set => selected = value; }
         //methods
         public abstract void Draw();
