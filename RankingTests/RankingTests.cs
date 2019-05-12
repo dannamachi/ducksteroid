2f46bb4773d1ca4026382754cbeaeccab4e440c2
dannamachi <serenita15@gmail.com>
2019-05-12 16:42:53 +1000

    Add more tests for ranking

--- RankingTests/RankingTests.cs
+++ RankingTests/RankingTests.cs
@@ -22,7 +22,7 @@ namespace Tests
         }
 
         [Test]
-        public void TestGetScore()
+        public void TestAddScore()
         {
             Ranking rank = new Ranking();
             Score score1 = new Score("ducktrial1", 20);
@@ -37,16 +37,23 @@ namespace Tests
         public void TestLoadFileFalse()
         {
             Ranking rank = new Ranking();
-            bool actual = rank.LoadFile("scorenope.txt");
-            Assert.AreEqual(false, actual, "Test ranking can load score from file");
+            StreamReader actual = rank.LoadFile("scorenope.txt");
+            Assert.IsNull(actual, "Test ranking can load score from file");
         }
         [Test]
-        public void TestLoadFileTrue()
+        public void TestGetContentFromFile()
         {
             Ranking rank = new Ranking();
             rank.LoadFile("scores.txt");
-            bool actual = (rank.Scores.Length == 2);
-            Assert.AreEqual(true, actual, "Test ranking can load score from file");
+            rank.LoadFile("scores.txt");
+            string line = rank.GetLine();
+            string actual = "";
+            while (line != null)
+            {
+                actual += line;
+                line = rank.GetLine();
+            }
+            Assert.AreEqual("22015", actual, "Test ranking can print file content");
         }
         [Test]
         public void TestSaveFile1()
@@ -67,6 +74,47 @@ namespace Tests
             reader.ReadLine();
             int actual = Convert.ToInt32(reader.ReadLine());
             Assert.AreEqual(15, actual, "Test ranking can save to file");
+        }
+        [Test]
+        public void TestSort()
+        {
+            Ranking rank = Setup();
+            rank.Sort();
+            bool actual = true;
+            for (int i = 0; i < rank.Scores.Length; i++)
+            {
+                for (int j = i + 1; j < rank.Scores.Length; i++)
+                {
+                    if (rank.Scores[i].Value > rank.Scores[j].Value) { actual = false; break; }
+                }
+            }
+            Assert.AreEqual(true, actual, "Test ranking can sort scores");
+        }
+        [Test]
+        public void TestCheckScoreCount()
+        {
+            Ranking rank = Setup();
+            Score score2 = new Score("ducktrial2", 15);
+            Score score3 = new Score("ducktrial3", 2);
+            rank.AddScore(score2);
+            rank.AddScore(score3);
+            rank.AddScore(score2);
+            rank.AddScore(score3);
+            rank.AddScore(score2);
+            rank.AddScore(score2);
+            rank.AddScore(score3);
+            rank.AddScore(score2);
+            int num = rank.Scores.Length;
+            bool sorted = true;
+            for (int i = 0; i < rank.Scores.Length; i++)
+            {
+                for (int j = i + 1; j < rank.Scores.Length; i++)
+                {
+                    if (rank.Scores[i].Value > rank.Scores[j].Value) { sorted = false; break; }
+                }
+            }
+            bool actual = (num == 10) && sorted;
+            Assert.AreEqual(true, actual, "Test ranking can add score with score count above 10");
         }
     }
 }
