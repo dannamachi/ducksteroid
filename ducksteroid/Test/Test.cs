aa42e404e2dff209775a9e4456470a6f8daa14dd
peterdu98 <peterhung998@gmail.com>
2019-05-12 17:54:32 +1000

    Add Test Project for Nunit

---
+++ ducksteroid/Test/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace Test
+{
+    [TestFixture ()]
+    public class Test
+    {
+        [Test ()]
+        public void TestIt ()
+        {
+            Assert.IsTrue (false);
+        }
+    }
+}
