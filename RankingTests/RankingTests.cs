95a660f54c8bfcbb7fb3832168163af39eac9099
dannamachi <serenita15@gmail.com>
2019-05-13 12:56:46 +1000

    Fix ranking tests

--- RankingTests/RankingTests.cs
+++ RankingTests/RankingTests.cs
@@ -22,10 +22,10 @@ namespace Tests
             return rank;
         }
 
-        //test: add score to a ranking obj
-        //expected: contain the score added (true)
+        //test: check if ranking adds the correct score
+        //expected: contain the correct score reference (true)
         [Test]
-        public void TestAddScore()
+        public void TestAddCorrectScore()
         {
             Ranking rank = Setup();
             Score score1 = new Score("ducktrial1", 20);
@@ -33,52 +33,6 @@ namespace Tests
             bool actual = rank.Scores.Contains(score1);
 
             Assert.AreEqual(true, actual, "Test ranking can add score");
-        }
-        //test: check if file can be loaded from
-        //expected: file is missing so cannot load (false)
-        [Test]
-        public void TestCheckFileMissing()
-        {
-            Ranking rank = new Ranking();
-            bool actual = rank.CheckFile("scorenope.txt");
-
-            Assert.AreEqual(false, actual, "Test ranking can detect missing file");
-        }
-        //test: check if file can be loaded from
-        //expected: file has more than 11 lines so cannot load (false)
-        [Test]
-        public void TestCheckFileValid1()
-        {
-            Ranking rank = new Ranking();
-            bool actual = rank.CheckFile("scoreover11.txt");
-
-            Assert.AreEqual(false, actual, "Test ranking can check for file validity");
-        }
-        //test: check if file can be loaded from
-        //expected: file has non-digit chars so cannot load (false)
-        [Test]
-        public void TestCheckFileValid2()
-        {
-            Ranking rank = new Ranking();
-            bool actual = rank.CheckFile("scorenondigit.txt");
-
-            Assert.AreEqual(false, actual, "Test ranking can check for file validity");
-        }
-        //test: check if ranking obj can read file content, line by line
-        //expected: concatenated string of first 3 lines of file 
-        [Test]
-        public void TestGetContentFromFile()
-        {
-            Ranking rank = new Ranking();
-            rank.LoadFile("scores.txt");
-            string line = rank.GetLine();
-            string actual = "";
-            while (line != null)
-            {
-                actual += line;
-                line = rank.GetLine();
-            }
-            Assert.AreEqual("22015", actual, "Test ranking can print file content");
         }
         //test: check if file is saved correctly
         //expected: number of scores in the ranking obj 
@@ -106,6 +60,32 @@ namespace Tests
             int actual = Convert.ToInt32(rank.GetLine().TrimEnd());
             int num = rank.Scores[1].Value;
             Assert.AreEqual(num, actual, "Test ranking can save to file");
+        }
+        //test: check if file exists and is in correct format to load from (less than 11 lines, contains only digit)
+        //expected: file exists and is in correct format (true)
+        [Test]
+        public void TestCheckFileValid()
+        {
+            Ranking rank = new Ranking();
+            bool actual = rank.CheckFile("scores.txt");
+
+            Assert.AreEqual(false, actual, "Test ranking can check for file validity");
+        }
+        //test: check if ranking obj can read file content, line by line
+        //expected: concatenated string of first 3 lines of file 
+        [Test]
+        public void TestGetContentFromFile()
+        {
+            Ranking rank = new Ranking();
+            rank.LoadFile("scores.txt");
+            string line = rank.GetLine();
+            string actual = "";
+            while (line != null)
+            {
+                actual += line;
+                line = rank.GetLine();
+            }
+            Assert.AreEqual("22015", actual, "Test ranking can print file content");
         }
         //test: check if ranking obj can sort score objs by value
         //expected: list of scores is sorted (true)
