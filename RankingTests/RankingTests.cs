4970b1685ca2ea4f0270c60409fc349f98f8e64b
dannamachi <serenita15@gmail.com>
2019-05-13 20:44:28 +1000

    Comment all ranking related stuff, add class files

--- RankingTests/RankingTests.cs
+++ RankingTests/RankingTests.cs
@@ -1,133 +1,133 @@
-using NUnit.Framework;
-using System.Collections.Generic;
-using System;
-using MyGame.src;
+//using NUnit.Framework;
+//using System.Collections.Generic;
+//using System;
+//using MyGame.src;
 
-namespace Tests
-{
-    public class RankingTests
-    {
-        //set-up method to return a ranking obj with 4 scores in it
-        public Ranking Setup()
-        {
-            Ranking rank = new Ranking();
-            Score score1 = new Score("ducktrial1", 20);
-            Score score2 = new Score("ducktrial2", 15);
-            Score score3 = new Score("ducktrial3", 2);
-            Score score4 = new Score("ducktrial4", 15);
-            rank.AddScore(score1);
-            rank.AddScore(score2);
-            rank.AddScore(score3);
-            rank.AddScore(score4);
-            return rank;
-        }
+//namespace Tests
+//{
+//    public class RankingTests
+//    {
+//        //set-up method to return a ranking obj with 4 scores in it
+//        public Ranking Setup()
+//        {
+//            Ranking rank = new Ranking();
+//            Score score1 = new Score("ducktrial1", 20);
+//            Score score2 = new Score("ducktrial2", 15);
+//            Score score3 = new Score("ducktrial3", 2);
+//            Score score4 = new Score("ducktrial4", 15);
+//            rank.AddScore(score1);
+//            rank.AddScore(score2);
+//            rank.AddScore(score3);
+//            rank.AddScore(score4);
+//            return rank;
+//        }
 
-        //test: check if ranking adds the correct score
-        //expected: contain the correct score reference (true)
-        [Test]
-        public void TestAddCorrectScore()
-        {
-            Ranking rank = Setup();
-            Score score1 = new Score("ducktrial1", 20);
-            rank.AddScore(score1);
-            bool actual = rank.Scores.Contains(score1);
+//        //test: check if ranking adds the correct score
+//        //expected: contain the correct score reference (true)
+//        [Test]
+//        public void TestAddCorrectScore()
+//        {
+//            Ranking rank = Setup();
+//            Score score1 = new Score("ducktrial1", 20);
+//            rank.AddScore(score1);
+//            bool actual = rank.Scores.Contains(score1);
 
-            Assert.AreEqual(true, actual, "Test ranking can add score");
-        }
-        //test: check if file is saved correctly
-        //expected: number of scores in the ranking obj
-        [Test]
-        public void TestSaveFile1()
-        {
-            Ranking rank = Setup();
-            rank.SaveScores("scores.txt");
-            rank.LoadFile("scores.txt");
-            string line = rank.GetLine();
-            int actual = Convert.ToInt32(line.TrimEnd());
-            int num = rank.Scores.Count;
-            Assert.AreEqual(num, actual, "Test ranking can save to file");
-        }
-        //test: check if file is saved correctly
-        //expected: value of 2nd score from top
-        [Test]
-        public void TestSaveFile2()
-        {
-            Ranking rank = Setup();
-            rank.SaveScores("scores.txt");
-            rank.LoadFile("scores.txt");
-            rank.GetLine();
-            rank.GetLine();
-            int actual = Convert.ToInt32(rank.GetLine().TrimEnd());
-            int num = rank.Scores[1].Value;
-            Assert.AreEqual(num, actual, "Test ranking can save to file");
-        }
-        //test: check if file exists and is in correct format to load from (less than 11 lines, contains only digit)
-        //expected: file exists and is in correct format (true)
-        [Test]
-        public void TestCheckFileValid()
-        {
-            Ranking rank = new Ranking();
-            bool actual = rank.CheckFile("scores.txt");
+//            Assert.AreEqual(true, actual, "Test ranking can add score");
+//        }
+//        //test: check if file is saved correctly
+//        //expected: number of scores in the ranking obj
+//        [Test]
+//        public void TestSaveFile1()
+//        {
+//            Ranking rank = Setup();
+//            rank.SaveScores("scores.txt");
+//            rank.LoadFile("scores.txt");
+//            string line = rank.GetLine();
+//            int actual = Convert.ToInt32(line.TrimEnd());
+//            int num = rank.Scores.Count;
+//            Assert.AreEqual(num, actual, "Test ranking can save to file");
+//        }
+//        //test: check if file is saved correctly
+//        //expected: value of 2nd score from top
+//        [Test]
+//        public void TestSaveFile2()
+//        {
+//            Ranking rank = Setup();
+//            rank.SaveScores("scores.txt");
+//            rank.LoadFile("scores.txt");
+//            rank.GetLine();
+//            rank.GetLine();
+//            int actual = Convert.ToInt32(rank.GetLine().TrimEnd());
+//            int num = rank.Scores[1].Value;
+//            Assert.AreEqual(num, actual, "Test ranking can save to file");
+//        }
+//        //test: check if file exists and is in correct format to load from (less than 11 lines, contains only digit)
+//        //expected: file exists and is in correct format (true)
+//        [Test]
+//        public void TestCheckFileValid()
+//        {
+//            Ranking rank = new Ranking();
+//            bool actual = rank.CheckFile("scores.txt");
 
-            Assert.AreEqual(false, actual, "Test ranking can check for file validity");
-        }
-        //test: check if ranking obj can read file content, line by line
-        //expected: concatenated string of first 3 lines of file
-        [Test]
-        public void TestGetContentFromFile()
-        {
-            Ranking rank = new Ranking();
-            rank.LoadFile("scores.txt");
-            string line = rank.GetLine();
-            string actual = "";
-            while (line != null)
-            {
-                actual += line;
-                line = rank.GetLine();
-            }
-            Assert.AreEqual("22015", actual, "Test ranking can print file content");
-        }
-        //test: check if ranking obj can sort score objs by value
-        //expected: list of scores is sorted (true)
-        [Test]
-        public void TestSort()
-        {
-            Ranking rank = Setup();
-            rank.Sort();
-            bool actual = true;
-            for (int i = 0; i < rank.Scores.Count; i++)
-            {
-                if (i > 0)
-                {
-                    if (rank.Scores[i] > rank.Scores[i - 1]) { actual = false; break; }
-                }
-                if (i < rank.Scores.Count - 1)
-                {
-                    if (rank.Scores[i] < rank.Scores[i + 1]) { actual = false; break; }
-                }
-            }
-            Assert.AreEqual(true, actual, "Test ranking can sort scores");
-        }
-        //test: check if ranking obj can remove and sort when there are more than 10 scores
-        //expected: 10 scores after adding and list of scores is sorted (true)
-        [Test]
-        public void TestCheckScoreCount()
-        {
-            Ranking rank = Setup();
-            Score score2 = new Score("ducktrial2", 15);
-            Score score3 = new Score("ducktrial3", 2);
-            rank.AddScore(score2);
-            rank.AddScore(score3);
-            rank.AddScore(score2);
-            rank.AddScore(score3);
-            rank.AddScore(score2);
-            rank.AddScore(score2);
-            rank.AddScore(score3);
-            rank.AddScore(score2);
-            int num = rank.Scores.Count;
+//            Assert.AreEqual(false, actual, "Test ranking can check for file validity");
+//        }
+//        //test: check if ranking obj can read file content, line by line
+//        //expected: concatenated string of first 3 lines of file
+//        [Test]
+//        public void TestGetContentFromFile()
+//        {
+//            Ranking rank = new Ranking();
+//            rank.LoadFile("scores.txt");
+//            string line = rank.GetLine();
+//            string actual = "";
+//            while (line != null)
+//            {
+//                actual += line;
+//                line = rank.GetLine();
+//            }
+//            Assert.AreEqual("22015", actual, "Test ranking can print file content");
+//        }
+//        //test: check if ranking obj can sort score objs by value
+//        //expected: list of scores is sorted (true)
+//        [Test]
+//        public void TestSort()
+//        {
+//            Ranking rank = Setup();
+//            rank.Sort();
+//            bool actual = true;
+//            for (int i = 0; i < rank.Scores.Count; i++)
+//            {
+//                if (i > 0)
+//                {
+//                    if (rank.Scores[i] > rank.Scores[i - 1]) { actual = false; break; }
+//                }
+//                if (i < rank.Scores.Count - 1)
+//                {
+//                    if (rank.Scores[i] < rank.Scores[i + 1]) { actual = false; break; }
+//                }
+//            }
+//            Assert.AreEqual(true, actual, "Test ranking can sort scores");
+//        }
+//        //test: check if ranking obj can remove and sort when there are more than 10 scores
+//        //expected: 10 scores after adding and list of scores is sorted (true)
+//        [Test]
+//        public void TestCheckScoreCount()
+//        {
+//            Ranking rank = Setup();
+//            Score score2 = new Score("ducktrial2", 15);
+//            Score score3 = new Score("ducktrial3", 2);
+//            rank.AddScore(score2);
+//            rank.AddScore(score3);
+//            rank.AddScore(score2);
+//            rank.AddScore(score3);
+//            rank.AddScore(score2);
+//            rank.AddScore(score2);
+//            rank.AddScore(score3);
+//            rank.AddScore(score2);
+//            int num = rank.Scores.Count;
 
-            bool actual = (num == 10);
-            Assert.AreEqual(true, actual, "Test ranking can add score with score count above 10");
-        }
-    }
-}
+//            bool actual = (num == 10);
+//            Assert.AreEqual(true, actual, "Test ranking can add score with score count above 10");
+//        }
+//    }
+//}
--- ducksteroid/src/Ranking.cs
+++ ducksteroid/src/Ranking.cs
@@ -1,131 +1,131 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.IO;
 
-namespace MyGame.src
-{
-    public class Ranking
-    {
-        //fields
-        private List<Score> _scores;
-        private StreamReader _reader;
-        //constructors
-        public Ranking()
-        {
-            _scores = new List<Score>();
-            _reader = null;
-        }
-        //properties
-        public List<Score> Scores { get => _scores; }
-        //methods
-        public bool SaveScores(string filepath)
-        {
-            StreamWriter writer;
-            try
-            {
-                writer = new StreamWriter(filepath);
-            }
-            catch (Exception e)
-            {
-                return false;
-            }
-            writer = new StreamWriter(filepath);
-            writer.WriteLine(_scores.Count);
-            foreach (Score sc in _scores)
-            {
-                writer.WriteLine(sc.Value);
-            }
-            writer.Close();
-            return true;
-        }
-        public void LoadScores(string filepath)
-        {
-            if (CheckFile(filepath))
-            {
-                LoadFile(filepath);
-                string line = GetLine();
-                int num;
-                Score score;
-                while (line != null)
-                {
-                    num = Convert.ToInt32(line.TrimEnd());
-                    score = new Score("", num);
-                    _scores.Add(score);
-                    line = GetLine();
-                }
-                _reader.Close();
-            }
-        }
-        public string GetLine()
-        {
-            if (_reader == null) { return null; }
-            return _reader.ReadLine();
-        }
-        public void LoadFile(string filepath)
-        {
-            _reader = new StreamReader(filepath);
-        }
-        private bool OnlyHasDigit(string line)
-        {
-            foreach (char letter in line)
-            {
-                if (letter < '0' || letter > '9') { return false; }
-            }
-            return true;
-        }
-        public bool CheckFile(string filepath)
-        {
-            StreamReader reader;
-            if (!File.Exists(filepath)) { return false; }
-            try
-            {
-                reader = new StreamReader(filepath);
-            }
-            catch (Exception e) {
-                return false;
-            }
-            reader = new StreamReader(filepath);
-            string line = reader.ReadLine();
-            int count = 0;
-            while (line != null)
-            {
-                if (count == 11) { return false; }
-                if (!OnlyHasDigit(line)) { return false; }
-                line = reader.ReadLine();
-                count += 1;
-            }
-            return true;
-        }
-        public void AddScore(Score sc)
-        {
-            _scores.Add(sc);
-            Sort();
-            if (Scores.Count > 10) {
-                List<Score> scores = new List<Score>();
-                for (int i = 0; i < 10; i++)
-                {
-                    scores.Add(Scores[i]);
-                }
-                _scores = scores;
-            }
-        }
-        public void Sort()
-        {
-            Score temp;
-            for (int i = 0; i < Scores.Count; i++)
-            {
-                for (int j = i + 1; j < Scores.Count; j++)
-                {
-                    if (Scores[i].Value < Scores[j].Value)
-                    {
-                        temp = Scores[i];
-                        Scores[i] = Scores[j];
-                        Scores[j] = temp;
-                    }
-                }
-            }
-        }
-    }
-}
+//namespace MyGame.src
+//{
+//    public class Ranking
+//    {
+//        //fields
+//        private List<Score> _scores;
+//        private StreamReader _reader;
+//        //constructors
+//        public Ranking()
+//        {
+//            _scores = new List<Score>();
+//            _reader = null;
+//        }
+//        //properties
+//        public List<Score> Scores { get => _scores; }
+//        //methods
+//        public bool SaveScores(string filepath)
+//        {
+//            StreamWriter writer;
+//            try
+//            {
+//                writer = new StreamWriter(filepath);
+//            }
+//            catch (Exception e)
+//            {
+//                return false;
+//            }
+//            writer = new StreamWriter(filepath);
+//            writer.WriteLine(_scores.Count);
+//            foreach (Score sc in _scores)
+//            {
+//                writer.WriteLine(sc.Value);
+//            }
+//            writer.Close();
+//            return true;
+//        }
+//        public void LoadScores(string filepath)
+//        {
+//            if (CheckFile(filepath))
+//            {
+//                LoadFile(filepath);
+//                string line = GetLine();
+//                int num;
+//                Score score;
+//                while (line != null)
+//                {
+//                    num = Convert.ToInt32(line.TrimEnd());
+//                    score = new Score("", num);
+//                    _scores.Add(score);
+//                    line = GetLine();
+//                }
+//                _reader.Close();
+//            }
+//        }
+//        public string GetLine()
+//        {
+//            if (_reader == null) { return null; }
+//            return _reader.ReadLine();
+//        }
+//        public void LoadFile(string filepath)
+//        {
+//            _reader = new StreamReader(filepath);
+//        }
+//        private bool OnlyHasDigit(string line)
+//        {
+//            foreach (char letter in line)
+//            {
+//                if (letter < '0' || letter > '9') { return false; }
+//            }
+//            return true;
+//        }
+//        public bool CheckFile(string filepath)
+//        {
+//            StreamReader reader;
+//            if (!File.Exists(filepath)) { return false; }
+//            try
+//            {
+//                reader = new StreamReader(filepath);
+//            }
+//            catch (Exception e) {
+//                return false;
+//            }
+//            reader = new StreamReader(filepath);
+//            string line = reader.ReadLine();
+//            int count = 0;
+//            while (line != null)
+//            {
+//                if (count == 11) { return false; }
+//                if (!OnlyHasDigit(line)) { return false; }
+//                line = reader.ReadLine();
+//                count += 1;
+//            }
+//            return true;
+//        }
+//        public void AddScore(Score sc)
+//        {
+//            _scores.Add(sc);
+//            Sort();
+//            if (Scores.Count > 10) {
+//                List<Score> scores = new List<Score>();
+//                for (int i = 0; i < 10; i++)
+//                {
+//                    scores.Add(Scores[i]);
+//                }
+//                _scores = scores;
+//            }
+//        }
+//        public void Sort()
+//        {
+//            Score temp;
+//            for (int i = 0; i < Scores.Count; i++)
+//            {
+//                for (int j = i + 1; j < Scores.Count; j++)
+//                {
+//                    if (Scores[i].Value < Scores[j].Value)
+//                    {
+//                        temp = Scores[i];
+//                        Scores[i] = Scores[j];
+//                        Scores[j] = temp;
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
