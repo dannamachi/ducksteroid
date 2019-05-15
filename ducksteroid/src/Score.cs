da9965c955e70d7be24f41fd92978b512b46e865
dannamachi <serenita15@gmail.com>
2019-05-16 03:07:57 +1000

    Implement Score and Ranking

---
+++ ducksteroid/src/Score.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace MyGame.src
+{
+    public class Score
+    {
+        private Counter _value;   
+        public int Value { get => _value.Value;}
+        
+        public Score(int number)
+        {
+            Counter something = new Counter();
+            _value = something;
+            for(int i = 0; i < number; i++)
+            {
+                _value.Increment();
+            }
+            
+        }
+    
+    }
+}
