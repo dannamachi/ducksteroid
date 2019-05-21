600fb56bf57b5668875d3615484299058e6cdf03
peterdu98 <peterhung998@gmail.com>
2019-05-21 16:53:42 +1000

    Delete Test folder and Create new test project TestMono

---
+++ ducksteroid/TestsMono/CounterTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using MyGame;
+
+namespace TestsMono
+{
+    [TestFixture ()]
+    public class CounterTest
+    {
+        private Counter _count;
+
+        [Test ()]
+        public void TestInitCounter ()
+        {
+            _count = new Counter ();
+            Assert.AreEqual (0, _count.Value);
+        }
+
+        [Test ()]
+        public void TestIncrement ()
+        {
+            _count = new Counter ();
+            _count.Increment ();
+            Assert.AreEqual (1, _count.Value);
+        }
+
+        [Test ()]
+        public void TestReset ()
+        {
+            _count = new Counter ();
+            _count.Increment ();
+            _count.Reset ();
+            Assert.AreEqual (0, _count.Value);
+        }
+    }
+}
